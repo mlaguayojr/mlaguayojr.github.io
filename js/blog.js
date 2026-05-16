2a77f2b2f189274ddb746aeffc082d2e862da769
mlaguayojr <mlaguayojr@gmail.com>
2026-05-16 05:31:59 -0400

    new content

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,16 @@
 const blogData = {
   posts: [
     {
-      title: "2026 face lift",
+      title: "Nouse eCalendar",
+      date: "2026-05-16",
+      href: "pages/nouse-anyuse-ecalendar/nouse-ecalendar.html",
+      summary: "Making an eCalendar stay connected to WiFi - you know, the bare minimum.",
+      tags: ["hacking","android"]
+    },
+    {
+      title: "2026 Portfolio Redesign",
       date: "2026-05-15",
+      href: "pages/2026-portfolio-redesign/2026-portfolio-redesign.html",
       summary: "🏗️ Under construction! Time to revamp this portfolio!",
       tags: ["design"]
     }
