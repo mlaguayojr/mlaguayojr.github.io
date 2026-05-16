e091fe8dc545ee1c21856b2c048fa21d71a37932
mlaguayojr <mlaguayojr@gmail.com>
2026-05-16 14:40:23 -0400

    fix year

--- js/education.js
+++ js/education.js
@@ -9,7 +9,7 @@ const educationData = {
         items: [
           "VP of Computer Science Club (2015 - 2016)",
           "Academic Advisor for Computer Science (Fall 2015 - Spring 2017)",
-          "Group Mentor for SCSU FemHack (2013)",
+          "Group Mentor for SCSU FemHack (2016)",
           "Soft Skills Analysis Project for CompSci and Business Dept.",
           "Participant of Top 50 CT Tech Council's Skills Challenge"
         ]
