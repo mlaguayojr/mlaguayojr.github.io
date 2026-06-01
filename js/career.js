0c578373c8e1cfb7a0f9f164be2a341bc2c5c5e4
mlaguayojr <mlaguayojr@gmail.com>
2026-06-01 17:25:26 -0400

    formatting cleanup and added new tags for career

--- js/career.js
+++ js/career.js
@@ -3,176 +3,269 @@ const careerData = {
     {
       name: "Solovis (Nasdaq)",
       superscript: "1",
-      footnote: "Solovis was wholly owned by Nasdaq until September of 2025, at which point it was sold to Insight Partners, a private equity firm specializing in software.",
+      footnote:
+        "Solovis was wholly owned by Nasdaq until September of 2025, at which point it was sold to Insight Partners, a private equity firm specializing in software.",
       location: "Remote",
       title: "Full Stack Software Engineer",
       duration: { start: "July 2022", end: "Present" },
-      skills: ["C#/.NET", "ASP.NET", "React", "TypeScript", "TanStack Query", "Entity Framework", "Azure", "Terraform", "Azure DevOps Pipelines", "Tailwind CSS", "SQL Server", "Jest", "Vite", "Airflow", "Docker", "Python", "RESTful API Design", "Confluence", "HTML", "CSS", "PowerShell", "Bash", "Visual Studio", "Git", "Postman", "Bruno", "GitHub Copilot", "Claude", "Cypress", "Playwright"],
+      skills: [
+        "C#/.NET",
+        "ASP.NET",
+        "React",
+        "TypeScript",
+        "TanStack Query",
+        "Entity Framework",
+        "Azure",
+        "Terraform",
+        "Azure DevOps Pipelines",
+        "Tailwind CSS",
+        "SQL Server",
+        "Jest",
+        "Vite",
+        "Airflow",
+        "Docker",
+        "Python",
+        "RESTful API Design",
+        "Confluence",
+        "HTML",
+        "CSS",
+        "PowerShell",
+        "Bash",
+        "Visual Studio",
+        "Git",
+        "Postman",
+        "Bruno",
+        "GitHub Copilot",
+        "Claude",
+        "Cypress",
+        "Playwright",
+        "RabbitMQ",
+        "MassTransit",
+        "nx.dev",
+      ],
       content: [
         {
-          topic: "Engineered Core Data Platform, a self-service ETL application empowering users to extract datasets from the Solovis ecosystem.",
+          topic:
+            "Engineered Core Data Platform, a self-service ETL application empowering users to extract datasets from the Solovis ecosystem.",
           subtopic: [
             "Implemented endpoints in RESTful API services to support key platform capabilities using ASP.NET and Entity Framework.",
             "Contributed to critical platform infrastructure in Microsoft Azure with Terraform.",
             "Authored and maintained CI/CD deployment code using Azure Pipelines for Core Data Platform API and UI codebases.",
-            "Integrated Core Data Platform UI with backend service capabilities with TanStack Query."
-          ]
+            "Integrated Core Data Platform UI with backend service capabilities with TanStack Query.",
+          ],
         },
         {
-          topic: "Maintained Fund Automated Sync Tool, a legacy Windows Forms application to maintain fund data within the Solovis ecosystem.",
+          topic:
+            "Maintained Fund Automated Sync Tool, a legacy Windows Forms application to maintain fund data within the Solovis ecosystem.",
           subtopic: [
             "Enhanced logging and telemetry around API integrations to enhance monitoring and troubleshooting capabilities.",
-            "Supported analysts by troubleshooting incorrect fund data between source and destination."
-          ]
+            "Supported analysts by troubleshooting incorrect fund data between source and destination.",
+          ],
         },
         {
-          topic: "Maintained Integration Hub, a data pipeline ETL system for custodial data integrations.",
+          topic:
+            "Maintained Integration Hub, a data pipeline ETL system for custodial data integrations.",
           subtopic: [
-            "Created new client feeds to deliver custodial data within the Solovis ecosystem."
-          ]
+            "Created new client feeds to deliver custodial data within the Solovis ecosystem.",
+          ],
         },
         {
-          topic: "Engineered Benchmark Blender, a scalable, rule-based system to manage custom benchmarks in Portfolio Analytics.",
+          topic:
+            "Engineered Benchmark Blender, a scalable, rule-based system to manage custom benchmarks in Portfolio Analytics.",
           subtopic: [
-            "Implemented rich React UI components to decrease developer time and provide consistent UX across the application."
-          ]
-        }
-      ]
+            "Implemented rich React UI components to decrease developer time and provide consistent UX across the application.",
+          ],
+        },
+      ],
     },
     {
       name: "Data-Mail",
       location: "Newington, CT",
       title: "Security Engineer",
       duration: { start: "October 2020", end: "June 2022" },
-      skills: ["Splunk", "Tenable.SC", "Linux", "Active Directory", "KACE SMA", "bash"],
+      skills: [
+        "Splunk",
+        "Tenable.SC",
+        "Linux",
+        "Active Directory",
+        "KACE SMA",
+        "Bash",
+      ],
       content: [
         {
-          topic: "Implemented Linux-based infrastructure for Splunk, a logging and SIEM system to lower operational costs and improve monitoring.",
+          topic:
+            "Implemented Linux-based infrastructure for Splunk, a logging and SIEM system to lower operational costs and improve monitoring.",
           subtopic: [
             "Replaced Windows-based hosts with Linux-based hosts to decrease licensing costs.",
-            "Created Dashboards and Alerts to capture hardware and software events across devices throughout company IT ecosystem."
-          ]
+            "Created Dashboards and Alerts to capture hardware and software events across devices throughout company IT ecosystem.",
+          ],
         },
         {
-          topic: "Implemented Tenable.SC, a vulnerability management platform to secure and monitor our company network.",
+          topic:
+            "Implemented Tenable.SC, a vulnerability management platform to secure and monitor our company network.",
           subtopic: [
-            "Implemented Dashboards to review vulnerabilities and plan remediation across company devices to support security needs and SLAs."
-          ]
+            "Implemented Dashboards to review vulnerabilities and plan remediation across company devices to support security needs and SLAs.",
+          ],
         },
         {
-          topic: "Performed enterprise system administration, security analysis, and group management.",
+          topic:
+            "Performed enterprise system administration, security analysis, and group management.",
           subtopic: [
             "Utilized KACE SMA for patching, asset management, and security across IT environments.",
-            "Maintained Active Directory Group Policies, matching job roles and responsibilities to reflect internal structural changes."
-          ]
-        }
-      ]
+            "Maintained Active Directory Group Policies, matching job roles and responsibilities to reflect internal structural changes.",
+          ],
+        },
+      ],
     },
     {
       name: "Data-Mail",
       location: "Newington, CT",
       title: "Jr. Software Developer & Production Support",
       duration: { start: "July 2018", end: "October 2020" },
-      skills: ["VB6", "VB.NET", "Microsoft Access", "SSRS", "SQL Server", "C#/.NET"],
+      skills: [
+        "VB6",
+        "VB.NET",
+        "Microsoft Access",
+        "SSRS",
+        "SQL Server",
+        "C#/.NET",
+      ],
       content: [
         {
-          topic: "Maintained and implemented requested features within home-grown legacy applications.",
+          topic:
+            "Maintained and implemented requested features within home-grown legacy applications.",
           subtopic: [
-            "Utilized Visual Basic 6, Visual Basic .NET, and Microsoft Access to add additional capabilities to internal software systems."
-          ]
+            "Utilized Visual Basic 6, Visual Basic .NET, and Microsoft Access to add additional capabilities to internal software systems.",
+          ],
         },
         {
-          topic: "Created reports across various production departments delivered by SSRS and e-mail.",
+          topic:
+            "Created reports across various production departments delivered by SSRS and e-mail.",
           subtopic: [
-            "Reported on production stage statuses, barcode integration with internal systems, and PDF delivery to executive production teams."
-          ]
+            "Reported on production stage statuses, barcode integration with internal systems, and PDF delivery to executive production teams.",
+          ],
         },
         {
-          topic: "Developed automation to expedite Mail Production workflow supporting other systems.",
+          topic:
+            "Developed automation to expedite Mail Production workflow supporting other systems.",
           subtopic: [
-            "Implemented capabilities to access, download, and scrape PDFs from USPS site to feed enterprise applications."
-          ]
-        }
-      ]
+            "Implemented capabilities to access, download, and scrape PDFs from USPS site to feed enterprise applications.",
+          ],
+        },
+      ],
     },
     {
       name: "The Network Support Company",
       location: "Danbury, CT",
       title: "Associate Developer",
       duration: { start: "June 2017", end: "April 2018" },
-      skills: ["PHP", "Yii", "JavaScript", "Three.js", "Python", "WebFocus", "SQL Server"],
+      skills: [
+        "PHP",
+        "Yii",
+        "JavaScript",
+        "Three.js",
+        "Python",
+        "WebFocus",
+        "SQL Server",
+      ],
       content: [
         {
-          topic: "Converted existing WebFocus, Python, and MSSQL reports for use in frontend dashboards using the Yii PHP framework."
+          topic:
+            "Converted existing WebFocus, Python, and MSSQL reports for use in frontend dashboards using the Yii PHP framework.",
         },
         {
-          topic: "Utilized Three.js to render and interact with 3D CAD files directly within a web page."
+          topic:
+            "Utilized Three.js to render and interact with 3D CAD files directly within a web page.",
         },
         {
-          topic: "Built a wrapper solution to integrate an internal CRM with a third-party vendor's software using PHP."
-        }
-      ]
+          topic:
+            "Built a wrapper solution to integrate an internal CRM with a third-party vendor's software using PHP.",
+        },
+      ],
     },
     {
       name: "O2 Safe Solutions",
       location: "Wallingford, CT",
       title: "IT Coordinator",
       duration: { start: "June 2015", end: "June 2017" },
-      skills: ["Java", "Python", "ADB", "Batch/MS-DOS", "Google Maps API", "Windows CE", "PocketPC"],
+      skills: [
+        "Java",
+        "Python",
+        "ADB",
+        "Batch/MS-DOS",
+        "Google Maps API",
+        "Windows CE",
+        "PocketPC",
+      ],
       content: [
         {
-          topic: "Created applications to replace manual, time-consuming processes across driver and invoicing workflows.",
+          topic:
+            "Created applications to replace manual, time-consuming processes across driver and invoicing workflows.",
           subtopic: [
             "Built a Java application to automate the configuration of PocketPCs for drivers, eliminating a tedious manual setup process.",
-            "Developed a Java GUI and Python shell tool to detect and correct user-based miscalculations in exported invoices."
-          ]
+            "Developed a Java GUI and Python shell tool to detect and correct user-based miscalculations in exported invoices.",
+          ],
         },
         {
           topic: "Automated device configuration and remote access management.",
           subtopic: [
             "Wrote Python shell scripts using ADB to automate the configuration process of Android tablets.",
-            "Created and maintained batch (MS-DOS) scripts for LogMeIn remote access management."
-          ]
+            "Created and maintained batch (MS-DOS) scripts for LogMeIn remote access management.",
+          ],
         },
         {
-          topic: "Utilized Google's Distance Matrix API to visualize area of coverage for branch locations."
+          topic:
+            "Utilized Google's Distance Matrix API to visualize area of coverage for branch locations.",
         },
         {
-          topic: "Help Desk/IT Support"
-        }
-      ]
+          topic: "Help Desk/IT Support",
+        },
+      ],
     },
     {
       name: "Nasdaq",
       location: "Shelton, CT",
       title: "InfoSec Intern",
       duration: { start: "May 2014", end: "August 2014" },
-      skills: ["Splunk", "Imperva", "Sourcefire", "SevOne", "DBNetworks", "CyberArk", "RedSeal"],
+      skills: [
+        "Splunk",
+        "Imperva",
+        "Sourcefire",
+        "SevOne",
+        "DBNetworks",
+        "CyberArk",
+        "RedSeal",
+      ],
       content: [
         {
-          topic: "Exposure to industry recognized access management and security solutions.",
+          topic:
+            "Exposure to industry recognized access management and security solutions.",
           subtopic: [
-            "Assessed tools spanning SIEM, database security, network monitoring, and privileged access management: Splunk, Imperva, Sourcefire, SevOne, DBNetworks, CyberArk, and RedSeal."
-          ]
-        }
-      ]
-    }
-  ]
+            "Assessed tools spanning SIEM, database security, network monitoring, and privileged access management: Splunk, Imperva, Sourcefire, SevOne, DBNetworks, CyberArk, and RedSeal.",
+          ],
+        },
+      ],
+    },
+  ],
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('/templates/career.html')
-    .then(res => res.text())
-    .then(template => {
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/templates/career.html")
+    .then((res) => res.text())
+    .then((template) => {
       const data = {
-        career: careerData.career.map(job => ({
+        career: careerData.career.map((job) => ({
           ...job,
-          content: job.content.map(item => ({
+          content: job.content.map((item) => ({
             ...item,
-            hasSubtopic: Array.isArray(item.subtopic) && item.subtopic.length > 0
-          }))
-        }))
+            hasSubtopic:
+              Array.isArray(item.subtopic) && item.subtopic.length > 0,
+          })),
+        })),
       };
-      document.getElementById('career').innerHTML = Mustache.render(template, data);
+      document.getElementById("career").innerHTML = Mustache.render(
+        template,
+        data,
+      );
     });
 });
